d054cee678d97281d4b89cdf44b82aa10aa7fa9a
LinuxLou2022 <116582084+LinuxLou2022@users.noreply.github.com>
2022-11-21 16:46:26 -0600

    test01 image loads

--- test01.py
+++ test01.py
@@ -7,6 +7,7 @@ import time
 print("test")
 
 box = Tk()
+box.geometry("600x600")
 Label(box, text="First").grid(row=0, sticky=W)
 Label(box, text="Second").grid(row=1, sticky=W)
 
@@ -14,5 +15,11 @@ e1 = Entry(box)
 e2 = Entry(box)
 
 e1.grid(row=0, column=1)
+e2.grid(row=1, column=2)
+
+pict2 = Image.open("S.jpg")
+img2 = ImageTk.PhotoImage(pict2)
+label = Label(image=img2)
+label.grid(row=1, column=3)
 
 box.mainloop()
